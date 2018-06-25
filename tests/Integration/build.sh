38285a1afdabfac8afaad2eaf41e232804c76076
Taras Dmytrykiv <t.dmytrykiv@gmail.com>
2018-06-25 21:31:55 +0300

    use ${CIRCLE_BRANCH} for determine branch

--- tests/Integration/build.sh
+++ tests/Integration/build.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
 APP_FOLDER=test-app
-SYMFONY_VERSION=3.3
+SYMFONY_VERSION=$1
+
+if [[ -z "$2" ]]; then
+    ZIPKIN_INSTRUMENTATION_SYMFONY_VERSION=dev-master
+else
+    ZIPKIN_INSTRUMENTATION_SYMFONY_VERSION=$2
+fi
 
 # Deletes old executions of the build
 rm -rf ${APP_FOLDER}
 
-composer create-project symfony/website-skeleton:${SYMFONY_VERSION} ${APP_FOLDER}
+composer create-project symfony/website-skeleton:${SYMFONY_VERSION} ${APP_FOLDER} || exit 1
 cd ${APP_FOLDER}
 
 # Includes zipkin-instrumentation-symfony to the composer.json of the app
@@ -15,7 +21,7 @@ cat composer.json.dist \
 | jq '.scripts["sync"] = ["rsync -arv --exclude=.git --exclude=tests/Integration --exclude=composer.lock --exclude=vendor ../../../ .zipkin-instrumentation-symfony"]' \
 | jq '.scripts["pre-install-cmd"] = ["@sync"]' \
 | jq '.scripts["pre-update-cmd"] = ["@sync"]' \
-| jq '.require["jcchavezs/zipkin-instrumentation-symfony"] = "dev-master"' \
+| jq --arg v "$ZIPKIN_INSTRUMENTATION_SYMFONY_VERSION" '.require["jcchavezs/zipkin-instrumentation-symfony"] = $v' \
 | jq '.repositories = [{"type": "path","url": "./.zipkin-instrumentation-symfony/","options": {"symlink": true}}]' > composer.json
 
 rm composer.lock
